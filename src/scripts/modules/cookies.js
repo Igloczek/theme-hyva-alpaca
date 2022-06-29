ced52c8b3dc076c1c5e9905b9b64e84723827ea2
Bartek Igielski <bartek.igielski@snow.dog>
2022-06-29 17:44:04 +0200

    Cleanup

--- src/scripts/modules/cookies.js
+++ src/scripts/modules/cookies.js
@@ -1,1 +1,3 @@
-// Some cookies stuff
+// I don't want to publish any Hyva code 1:1
+// but you can put there code from 'magento2-theme-module/src/view/frontend/templates/page/js/cookies.phtml'
+// and it will become part of the JS bundle.
